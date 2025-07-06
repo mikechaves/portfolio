263ab47ed93ec81a75c9bb68e42072aac43acd8b
Michael Chaves <163567591+mikechaves@users.noreply.github.com>
2025-07-06 12:22:11 -0700

    fix: use event name as image alt

--- components/recent-highlights.tsx
+++ components/recent-highlights.tsx
@@ -56,7 +56,7 @@ export function RecentHighlights() {
                     h.event
                   )}`
                 }
-                alt={`Thumbnail for ${h.event}`}
+                alt={h.event}
                 fill
                 className="object-cover rounded"
               />
